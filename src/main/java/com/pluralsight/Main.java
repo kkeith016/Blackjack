8fbca7c53f29d6e6b3651e8945e6803fcaba8805
Kylee <kkeith.biz@gmail.com>
2025-10-23 14:07:47 -0400

    working in main

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,16 @@
 package com.pluralsight;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
 
+        System.out.println("Welcome to Blackjack!");
+        System.out.print("Enter number of players: ");
+        int numPlayers = scanner.nextInt();
+        scanner.nextLine();
     }
 }
